b09710cbeff0b5c108426afa84525773c48a5c68
Ricardo Cortes <ricardojcortes8@gmail.com>
2025-05-30 11:49:32 -0500

    Reformat SQL code

--- data_analyst_project.sql
+++ data_analyst_project.sql
@@ -3,121 +3,118 @@
 -------------------------------------------------------------------------------------------------------
 --1)How many rows are in the data_analyst_jobs table? 1793
 -------------------------------------------------------------------------------------------------------
-select count(*)
-from data_analyst_jobs;
+SELECT COUNT(*)
+FROM data_analyst_jobs;
 -------------------------------------------------------------------------------------------------------
 --2)What company is associated with the job posting on the 10th row? Exon Mobile
 -------------------------------------------------------------------------------------------------------
-select company
-from data_analyst_jobs
-limit 10;
+SELECT company
+FROM data_analyst_jobs
+LIMIT 10;
 -------------------------------------------------------------------------------------------------------
 --3)How many postings are in Tennessee? 21 How many are there in either Tennessee or Kentucky? 27
 -------------------------------------------------------------------------------------------------------
-select count(*)
-from data_analyst_jobs
-where location = 'TN';
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location = 'TN';
 
 --Option A
-select count(*)
-from data_analyst_jobs
-where location = 'TN' or location = 'KY';
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location = 'TN' or location = 'KY';
 
 --Option B
-select count(*)
-from data_analyst_jobs
-where location IN('TN', 'KY');
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location IN('TN', 'KY');
 
 -------------------------------------------------------------------------------------------------------
 --4)How many postings in Tennessee have a star rating above 4?
 -------------------------------------------------------------------------------------------------------
-select count(*)
-from data_analyst_jobs
-where location = 'TN'
-		and star_rating > 4;
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location = 'TN'
+		AND star_rating > 4;
 -------------------------------------------------------------------------------------------------------
---5)How many postings in the dataset have a review count between 500 and 1000?
+--5)How many postings in the dataset have a review COUNT between 500 AND 1000?
 ------------------------------------------------------------------------------------------------------- 
-select count(*)
-from data_analyst_jobs
-where review_count between 500 and 1000;
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE review_COUNT BETWEEN 500 AND 1000;
 -------------------------------------------------------------------------------------------------------
 --6)Show the average star rating for companies in each state. 
---The output should show the state as `state` and the average rating for the state as `avg_rating`. 
+--The output should show the state as `state` AND the average rating for the state as `avg_rating`. 
 --Which state shows the highest average rating? NE
 -------------------------------------------------------------------------------------------------------
-select location as state, 
-	   round(avg(star_rating),1) as avg_rating
-from data_analyst_jobs
-where star_rating is not null
-group by 1
-order by 2 DESC;
+SELECT location AS state, 
+	   round(AVG(star_rating),1) AS avg_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+GROUP BY 1
+ORDER BY 2 DESC;
 -------------------------------------------------------------------------------------------------------
---7)Select unique job titles from the data_analyst_jobs table. How many are there? 881
+--7)SELECT unique job titles FROM the data_analyst_jobs table. How many are there? 881
 -------------------------------------------------------------------------------------------------------
-select distinct title
-from data_analyst_jobs;
+SELECT distinct title
+FROM data_analyst_jobs;
 -------------------------------------------------------------------------------------------------------
 --8)How many unique job titles are there for California companies? 230
 -------------------------------------------------------------------------------------------------------
-select count(distinct title)
-from data_analyst_jobs
-where location = 'CA';
+SELECT COUNT(distinct title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
 -------------------------------------------------------------------------------------------------------
---9)Find the name of each company and its average star rating for all companies that have more than 
+--9)Find the name of each company AND its average star rating for all companies that have more than 
 --5000 reviews across all locations. How many companies are there with more that 5000 
 --reviews across all locations? 42
 -------------------------------------------------------------------------------------------------------
-select company, 
-	   round(avg(star_rating),1) as avg_rating
-from data_analyst_jobs
-where star_rating is not null
-	and review_count > 5000
-group by 1;
+SELECT company, 
+	   round(AVG(star_rating),1) AS avg_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+	AND review_COUNT > 5000
+GROUP BY 1;
 -------------------------------------------------------------------------------------------------------
---10)Add the code to order the query in #9 from highest to lowest average star rating. 
+--10)Add the code to order the query in #9 FROM highest to lowest average star rating. 
 --Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? 
 --What is that rating? Kaiser Permanente with a 4.2 however it is worth mentioning that there were 6 company in total 
---tied with the same avg_rating and I used the review count as tie breaker (8164)
+--tied with the same avg_rating AND I used the review COUNT as tie breaker (8164)
 -------------------------------------------------------------------------------------------------------
-select company,
-	   review_count,
-	   round(avg(star_rating),1) as avg_rating
-from data_analyst_jobs
-where star_rating is not null
-	and review_count > 5000
-group by 1, 2
-order by 3 DESC, 2 DESC;
+SELECT company,
+	   review_COUNT,
+	   round(AVG(star_rating),1) AS avg_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+	AND review_COUNT > 5000
+GROUP BY 1, 2
+ORDER BY 3 DESC, 2 DESC;
 
 -------------------------------------------------------------------------------------------------------
 --11)Find all the job titles that contain the word ‘Analyst’. 
 --How many different job titles are there? 1669
 -------------------------------------------------------------------------------------------------------
-select title
-from data_analyst_jobs
-where title ILIKE '%analyst%';
+SELECT title
+FROM data_analyst_jobs
+WHERE title ILIKE '%analyst%';
 -------------------------------------------------------------------------------------------------------
 --12)How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? 
 --What word do these positions have in common?
 -------------------------------------------------------------------------------------------------------
-select title
-from data_analyst_jobs
-where title not ilike '%analyst%' AND title not ilike '%analytics%';
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT ILIKE '%analyst%' AND title NOT ILIKE '%analytics%';
 -------------------------------------------------------------------------------------------------------
 --Bonus)
 -------------------------------------------------------------------------------------------------------
-select domain, count(*)
-from(select *
-	 from data_analyst_jobs
-	 where skill ilike '%sql%'
-		and days_since_posting > 21
-		and domain is not null
-	 order by days_since_posting DESC)
-group by 1
-order by 2 DESC
-limit 4
-
+SELECT domain, COUNT(*)
+FROM(SELECT *
+	 FROM data_analyst_jobs
+	 WHERE skill ILIKE '%sql%'
+		AND days_since_posting > 21
+		AND domain IS NOT NULL
+	 ORDER BY days_since_posting DESC)
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 4
 
 -------------------------------------------------------------------------------------------------------
-
-
